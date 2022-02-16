762e39230d08111088aa202dd490179af8bd4a06
Usova Marina Andreevna <oppabang@mail.ru>
2022-02-16 10:02:12 +0300

    add another classes by example, fix some ideas

--- IntrodactionIntoOOP/Game.h
+++ IntrodactionIntoOOP/Game.h
@@ -2,39 +2,108 @@
 #include <iostream>
 #include <string>
 
-/*
-1. различие между классом и структурой - возможность сокрытия информации,
-контролируемый доступ - сеттеры и геттеры.
-2. с чего начинается работа над классом - определение набора
-классов и их полей.
-2.5. вывод на экран.
-3. конструкторы!!!! 3 штуки!!!
-4. выделение классов: что класс, а что структура.
-5. сокрытие важных данных, их обрабока в сеттерах и геттерах
-6. а нужно ли наследование? - организация родительского класса
-7. protected - для доступа поля родителя в потомке.
-8. реализуете нужный функционал!
-*/
+class Weapon;
+class Gun;
+class Bow;
+class Axe;
 
-// Weapons
+class Character;
+class Human;
+class Archer;
+class Warrior;
 
-struct Gun {
+/*************** Weapons ****************/
+
+class Weapon {
+protected:
   std::string name;
   double damage;
-  int cartridges;
+
+public:
+  Weapon() {
+    name = "Foo";
+    damage = 0;
+  };
+
+  Weapon(std::string _name, double _damage) {
+    name = _name;
+    damage = _damage;
+  };
+
+  Weapon(const Weapon &w) {
+    name = w.name;
+    damage = w.damage;
+  };
+
+  void print() {
+    std::cout << "Weapon's name: " << name << std::endl <<
+      "Weapon's damage: " << damage << std::endl;
+  };
 };
 
-struct Bow {
-  std::string name;
-  double damage;
+class Gun : public Weapon {
+  int cartridges;
+
+public:
+  Gun() {
+    name = "PlasmaGun";
+    damage = 20.0;
+    cartridges = 15;
+  };
+
+  Gun(std::string _name, double _damage, int _cartridges) : Weapon(_name, _damage) {
+    cartridges = _cartridges;
+  };
+
+  Gun(const Gun &a) : Weapon(a.name, a.damage) {
+    cartridges = a.cartridges;
+  };
+
+  void print() {
+    Weapon::print();
+    std::cout << "Weapon's cartridge count: " << cartridges << std::endl;
+  };
+
+  friend class Human;     // открывает доступ к скрытым поля указанному классу
+};
+
+struct Bow : public Weapon {
   int arrows;
   double extraDamage;
   int time;
+
+public:
+  Bow() {
+    name = "PlasmaGun";
+    damage = 20.0;
+    arrows = 10;
+    extraDamage = 5.0;    // урон в секунду
+    time = 5;             // в секундах
+  };
+
+  Bow(std::string _name, double _damage, int _arrows, double _extraDamage, int _time) : Weapon(_name, _damage) {
+    arrows = _arrows;
+    extraDamage = _extraDamage;
+    time = _time;
+  };
+
+  Bow(const Bow &a) : Weapon(a.name, a.damage) {
+    arrows = a.arrows;
+    extraDamage = a.extraDamage;
+    time = a.time;
+  };
+
+  void print() {
+    Weapon::print();
+    std::cout << "Weapon's arrows count: " << arrows << std::endl;
+    std::cout << "Weapon's extra damage per second: " << extraDamage << std::endl;
+    std::cout << "Weapon's effect time: " << time << std::endl;
+  };
+
+  friend class Archer;
 };
 
-class Axe {
-  std::string name;
-  double damage;
+class Axe : public Weapon{
   int strenght;
 
 public:
@@ -44,37 +113,24 @@ public:
     strenght = 100.0;
   };
 
-  Axe(std::string _name, double _damage, int _strenght) {
-    name = _name;
-    damage = _damage;
+  Axe(std::string _name, double _damage, int _strenght) : Weapon(_name, _damage) {
     strenght = _strenght;
   };
 
-  Axe(const Axe &a) {
-    name = a.name;
-    damage = a.damage;
+  Axe(const Axe &a) : Weapon(a.name, a.damage) {
     strenght = a.strenght;
   };
 
-  // геттеры
+  /*
+
+  // Пока не требуются по реализации
+
+  // геттер (пример)
   std::string getName() {
     return name;
   };
 
-  double getDamage() {
-    return damage;
-  };
-
-  int getStrenght() {
-    return strenght;
-  };
-
-  // сеттер
-  void setName(std::string _name) {
-    // может быть огромная проверка - можно ли так сделать
-    name = _name;
-  }
-
+  // сеттер (пример)
   void setDamage(double _damage) {
     if (_damage < 0) {
       std::cout << "FAILED!!!!" << std::endl;
@@ -85,13 +141,17 @@ public:
       damage = _damage;
   }
 
-  void setStrenght(int _strenght) {
-    strenght = _strenght;
-  }
+  */
 
+  void print() {
+    Weapon::print();
+    std::cout << "Weapon's strenght: " << strenght << std::endl;
+  };
+
+  friend class Warrior;
 };
 
-// Characters
+/***************** Characters ****************/
 
 class Character {
 protected:
@@ -125,64 +185,126 @@ public:
     health = w.health;
   };
 
-  void print_character_info() {
+  void print() {
     std::cout << "Name: " << name << std::endl <<
       "Gender: " << gender << std::endl <<
       "Power: " << power << std::endl <<
       "Health: " << health << std::endl;
    };
-
-  void attack(Character x);
 };
 
-struct Human {
-  std::string name;
-  std::string  gender;
-  double power;
-  double health;
+class Human : Character {
   Gun weapon;
   int technics;
+
+public:
+  Human() {
+    name = "James Bond";
+    gender = "male";
+    power = 10.0;
+    health = 100.0;
+    // + оружие по умолчанию создаётся само
+    technics = 30.0;
+  };
+
+  Human(std::string _name, std::string  _gender, double _power,
+    double _health, std::string _nameW, double _damage, int _cartridges,
+    double _technics) : Character(_name, _gender, _power, _health) {
+    weapon.name = _nameW;
+    weapon.damage = _damage;
+    weapon.cartridges = _cartridges;
+    technics = _technics;
+  };
+
+  Human(std::string _name, std::string  _gender, double _power,
+    double _health, Gun _weapon, double _technics)
+    : Character(_name, _gender, _power, _health) {
+    weapon = _weapon;
+    technics = _technics;
+  };
+
+  Human(const Human &w) : Character(w.name, w.gender, w.power, w.health) {
+    weapon = w.weapon;
+    technics = w.technics;
+  };
+
+  void print() {
+    Character::print();
+    std::cout << "Technics: " << technics << std::endl;
+    weapon.print();
+  };
 };
 
-struct Archer {
-  std::string name;
-  std::string  gender;
-  double power;
-  double health;
+class Archer : Character {
   Bow weapon;
   int dexterity;
+
+public:
+  Archer() {
+    name = "Legolas";
+    gender = "male";
+    power = 0.0;
+    health = 100.0;
+    dexterity = 15.0;
+  };
+
+  Archer(std::string _name, std::string  _gender, double _power,
+    double _health, std::string _nameW, double _damage, int _arrows, double _extraDamage,
+    int _time, double _dexterity) : Character(_name, _gender, _power, _health) {
+
+    weapon.name = _nameW;
+    weapon.damage = _damage;
+    weapon.arrows = _arrows;
+    weapon.extraDamage = _extraDamage;
+    weapon.time = _time;
+
+    dexterity = _dexterity;
+  };
+
+  Archer(std::string _name, std::string  _gender, double _power,
+    double _health, Bow _weapon, double _dexterity)
+    : Character(_name, _gender, _power, _health) {
+    weapon = _weapon;
+    dexterity = _dexterity;
+  };
+
+  Archer(const Archer &w) : Character(w.name, w.gender, w.power, w.health) {
+    weapon = w.weapon;
+    dexterity = w.dexterity;
+  };
+
+  void print() {
+    Character::print();
+    std::cout << "Dexterity: " << dexterity << std::endl;
+    weapon.print();
+  };
 };
 
 class Warrior : Character {
-private:
   Axe weapon;
   double armor;
 
 public:
-  // конструктор по умолчанию
   Warrior() {
     name = "Garrosh Hellscream";
     gender = "male";
     power = 10.0;
     health = 100.0;
     armor = 30.0;
-    /* Axe a;
-    weapon = a;*/
   };
 
-  // конструктор инициализации расширенный
+  // конструктор инициализации (расширенный)
   Warrior(std::string _name, std::string  _gender, double _power,
   double _health, std::string _nameW, double _damage, int _strenght,
   double _armor) : Character(_name, _gender, _power, _health) {
     
-    weapon.setName(_nameW);
-    weapon.setDamage(_damage);
-    weapon.setStrenght(_strenght);
+    weapon.name = _nameW;
+    weapon.damage = _damage;
+    weapon.strenght = _strenght;
 
     armor = _armor;
   };
 
-  // конструктор инициализации
   Warrior(std::string _name, std::string  _gender, double _power,
     double _health, Axe _weapon, double _armor)
     : Character(_name, _gender, _power, _health) {
@@ -190,23 +312,15 @@ public:
     armor = _armor;
   };
 
-  // конструктор копирования
   Warrior(const Warrior &w) : Character(w.name, w.gender, w.power, w.health) {
     weapon = w.weapon;
     armor = w.armor;
   };
 
-  void print_character_info() {
-    Character::print_character_info();
-    std::cout << "Armor: " << armor << std::endl <<
-      /*
-      "Weapon: " << w.getName() << std::endl <<
-      "Weapon's damage: " << w.getDamage() << std::endl <<
-      "Weapon's strenght: " << w.getStrenght() << std::endl;
-      */
-      "Weapon: " << weapon.getName() << std::endl <<
-      "Weapon's damage: " << weapon.getDamage() << std::endl <<
-      "Weapon's strenght: " << weapon.getStrenght() << std::endl;
+  void print() {
+    Character::print();
+    std::cout << "Armor: " << armor << std::endl;
+    weapon.print();
   };
 
 };
--- IntrodactionIntoOOP/main.cpp
+++ IntrodactionIntoOOP/main.cpp
@@ -2,27 +2,30 @@
 
 int main() {
   // вызов конструктора по умолчанию
-  Warrior user_0001;
+  Human user_0001;
 
-  //Axe user_0001_weapon;
-  Axe user_0002_weapon("Knife", 3.0, 20);
-  //Axe user_0003_weapon(user_0002_weapon);
-
-  // вызов конструктора инициализации
-  Warrior user_0002("Grom", "male", 20.0, 100.0, user_0002_weapon, 30.0);
-
-  // вызов расширенного конструктора инициализации
-  Warrior user_0004("Grom", "male", 20.0, 100.0, "SUPER AXE", -60.0, 100.0, 30.0);
+  // вызов конструкторов инициализации
+  Gun user_0002_weapon("Vinchester", 3.0, 20);
+  Human user_0002("John Wick", "male", 20.0, 100.0, user_0002_weapon, 30.0);
 
   // вызов конструктора копировани€
-  Warrior user_0003(user_0001);
+  Human user_0003(user_0001);
 
-  /// user_0002.health = 10000000; Ќ≈Ћ№«я!!!!!
+  // вызов расширенного конструктора инициализации
+  Human user_0004("Lebowski", "male", 10.0, 80.0, "Shotgun", 60.0, 100.0, 0.0);
 
-  user_0001.print_character_info();
-  user_0002.print_character_info();
-  user_0003.print_character_info();
-  user_0004.print_character_info();
+  // вывод на экран
+  std::cout << "USER 1" << std::endl;
+  user_0001.print();
+
+  std::cout << std::endl << "USER 2" << std::endl;
+  user_0002.print();
+
+  std::cout << std::endl << "USER 3" << std::endl;
+  user_0003.print();
+
+  std::cout << std::endl << "USER 4" << std::endl;
+  user_0004.print();
 
   system("pause");
   return 0;
