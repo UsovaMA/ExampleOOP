0d6fedb3aae5540d2ae1f4b2b037d35166f52e09
Marina Usova <usova@itmm.unn.com>
2022-03-23 08:25:43 +0300

    define classes system and main methods

--- DeskApp/Desk/Desk.h
+++ DeskApp/Desk/Desk.h
@@ -10,3 +10,58 @@
  * 3. App - свойства - все конфигурации, все платформы - —/—++, общие, каталоги включаемых файлов
       -> путь до Desk.h
  */
+
+class Product {
+  std::string code;
+  std::string name;
+  int price;
+  int discount;
+  int count;
+public:
+  // конструкторы
+  Product();
+  Product(std::string _code, std::string _name, int _price, int _discount, int _count);
+
+  // методы
+
+  // перегрузки
+  bool operator==(const Product& prod);
+  friend std::ostream& operator<< (std::ostream& out, const Product& prod);
+
+  friend class Busket;
+};
+
+class Assortment {
+  Product* all;
+  int count;
+
+public:
+  // конструктор - по умолчанию читаем из файла
+  Assortment();
+};
+
+typedef Product* ProductLink;
+
+class Busket {
+  ProductLink* goods;
+  int size;
+  int count;
+  ProductLink last;
+
+public:
+  // конструктор - по умолчанию пуста€ корзина
+  Busket();
+
+  // деструктор
+  ~Busket();
+
+  // методы
+  void scan(std::string _code);
+  void showLastProduct();
+  void addToCheck();
+  std::string makeCheck();
+  int calculateTotalCost();
+
+private: // делаем часть методов скрытыми (служебными)
+  // этот блок отвественный за реализацию может прорабатывать сам
+};
